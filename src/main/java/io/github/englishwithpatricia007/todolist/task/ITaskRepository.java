86a101afd953d9e49d032c7202bf4a3f10c9695d
Patricia <englishwithpatricia007@gmail.com>
2025-05-22 08:23:46 -0300

    Listando tarefas do usuário

--- src/main/java/io/github/englishwithpatricia007/todolist/task/ITaskRepository.java
+++ src/main/java/io/github/englishwithpatricia007/todolist/task/ITaskRepository.java
@@ -1,9 +1,11 @@
 package io.github.englishwithpatricia007.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  List<TaskModel> findByIdUser(UUID idUser);
 
 }
